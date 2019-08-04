ad1f9979b33503950c73640299fdaf2462694489
Braden <drab3n@gmail.com>
2019-08-03 23:32:59 -0600

    Improved some_stats

--- some_stats.py
+++ some_stats.py
@@ -7,12 +7,12 @@ def run():
     data = []
 
     while True:
-        option = 0
         print("[1] - Enter Data\n" +
               "[2] - View Data\n" +
               "[3] - Delete Data\n" +
               "[4] - Compute Statistics on Dataset\n" +
-              "[5] - Exit")
+              "[5] - Compute Statistics on Specific Data Point\n"
+              "[6] - Exit")
 
         selection = input("Select an option: ")
         print()
@@ -26,8 +26,10 @@ def run():
             elif selection == 3:
                 delete_data(data)
             elif selection == 4:
-                compute(data)
+                compute_set(data)
             elif selection == 5:
+                compute_point(data)
+            elif selection == 6:
                 print("Exiting!")
                 exit(0)
             else:
@@ -84,10 +86,10 @@ def delete_data(data_set):
                 else:
                     print("That value is not in the data set!")
             except ValueError:
-                print("Not a valid input! Try again.")
+                print("Not a valid input! Try again.\n")
 
 
-def compute(data_set):
+def compute_set(data_set):
 
     def mean():
         if len(data_set) == 0:
@@ -101,9 +103,6 @@ def compute(data_set):
         if len(sorted_set) == 0:
             return "NO MEDIAN"
         elif len(sorted_set) % 2 == 0:
-            # even
-            # [0, 1, 2, 3, 4, 5]
-            # [0, 1, 2, 3, 4, 5, 6, 7, 8]
             return (sorted_set[int(len(sorted_set) // 2 - 1)] + sorted_set[int(len(sorted_set) // 2)]) / 2
         else:
             return sorted_set[len(sorted_set) // 2]
@@ -152,6 +151,81 @@ def compute(data_set):
           "Variance: " + str(variance()) + "\n" +
           "Standard Deviation: " + str(standard_deviation()) + "\n" +
           "Coefficient of Variation: " + str(coefficient_of_variation()) + "\n")
+
+
+def compute_point(data_set):
+    sorted_set = sorted(data_set)
+
+    def calc_percentile(position):
+        if 0 > position >= len(sorted_set):
+            print("Invalid input! Try again\n")
+            return "INVALID"
+
+        return round(((position - 1) / (len(data_set) - 1)), 2) * 100
+
+    def calc_position(percentile):
+        if percentile < 0 or percentile > 100:
+            print("Invalid input! Try again\n")
+            return "INVALID"
+
+        return round((percentile * (len(data_set) - 1)) / 100)
+
+    if len(data_set) == 0:
+        print("Cannot compute any information on empty data set!\n")
+        return
+
+    while True:
+        print("[1] - Calculate the percentile of a given position\n" +
+              "[2] - Calculate the position of a given value\n" +
+              "[3] - Cancel")
+
+        selection = input("Select an option: ")
+        print()
+
+        try:
+            selection = int(selection)
+            if selection == 1:
+                print("Positions corresponding to their data values:")
+                for i in range(0, len(sorted_set)):
+                    print("[" + str(i + 1) + "] - " + str(sorted_set[i]))
+
+                selection = input("Provide a position: ")
+                try:
+                    selection = int(selection)
+                    if 0 < selection <= len(sorted_set):
+                        calculated_percentile = calc_percentile(selection)
+                        print("The item: [" + str(sorted_set[selection - 1]) +
+                              "] at position [" + str(selection) +
+                              "] corresponds to percentile [" + str(calculated_percentile) + "].")
+                    else:
+                        print("Value selected out of bounds! Try again.\n")
+                except ValueError:
+                    print("Not a valid input! Try again.\n")
+            elif selection == 2:
+                selection = input("Provide a percentile: ")
+                try:
+                    selection = int(selection)
+                    if 0 <= selection <= 100:
+                        calculated_position = calc_position(selection)
+                        if 0 <= selection <= 100:
+                            if round(selection) == selection:
+                                print("The percentile [" + str(selection) +
+                                      "] corresponds to the item: [" +
+                                      str(sorted_set[selection]) + "] at position: [" + str(calculated_position) + "].")
+                            else:
+                                print("The percentile [" + str(selection) +
+                                      "] corresponds to position: [" + str(calculated_position) + "].")
+                    else:
+                        print("Not a valid percentile! Try again.")
+                except ValueError:
+                    print("Not a valid input! Try again.\n")
+            elif selection == 3:
+                print("Exiting menu!")
+                return
+            else:
+                print("Not a valid input! Try again.\n")
+        except ValueError:
+            print("Not a valid input! Try again.\n")
 
 
 if __name__ == '__main__':
